5e8f427d57c6c9f651e9467eb3b6ef0c56d65ea4
Joel Edwards <joeledwards@gmail.com>
2021-12-04 04:00:34 -0700

    feat: Day 5 prep

--- 2021/src/main/scala/com/buzuli/advent/days/day5.scala
+++ 2021/src/main/scala/com/buzuli/advent/days/day5.scala
@@ -1,11 +1,48 @@
 package com.buzuli.advent.days
 
 import com.buzuli.advent.{AdventContext, AdventDay, DayResult}
+import com.buzuli.util.Time
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object day5 extends AdventDay(5) {
   override def _execute(context: AdventContext)(implicit ec: ExecutionContext): Future[DayResult] = {
-    Future.successful(failure("Not implemented"))
+    val r1 = puzzle1
+    val r2 = puzzle2
+    for {
+      p1 <- r1
+      p2 <- r2
+    } yield {
+      success(s"""Puzzle1 => ${p1} | Puzzle2 => ${p2}""")
+      failure("Not Implemented")
+    }
+  }
+
+  def puzzle1(implicit ec: ExecutionContext): Future[Int] = Future {
+    val (duration, value) = Time.timing {
+      1
+    }
+
+    logger.info(s"${name} Puzzle 1 => ${value} (${Time.prettyDuration(duration)})")
+
+    value
+  }
+
+  def puzzle2(implicit ec: ExecutionContext): Future[Int] = Future {
+    val (duration, value) = Time.timing {
+      2
+    }
+
+    logger.info(s"${name} Puzzle 1 => ${value} (${Time.prettyDuration(duration)})")
+
+    value
+  }
+
+  object data {
+
+    val raw: String =
+      """
+        |
+        |""".stripMargin
   }
 }
