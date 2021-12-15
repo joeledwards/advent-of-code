14f41e0ae876c90440498424eefea08b3642e612
Joel Edwards <joeledwards@gmail.com>
2021-12-15 13:15:55 -0700

    feat: started on day 15

---
+++ 2021/src/main/scala/com/buzuli/advent/days/day15.scala
@@ -0,0 +1,36 @@
+package com.buzuli.advent.days
+
+import com.buzuli.advent.{AdventContext, AdventDay}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+
+object day15 extends AdventDay(15) {
+  override def puzzles(implicit ec: ExecutionContext): List[AdventContext => Future[String]] = {
+    List(puzzle1, puzzle2)
+  }
+
+  def puzzle1(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future {
+    "--"
+  }
+
+  def puzzle2(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future {
+    "--"
+  }
+
+
+
+  lazy val sample: List[String] = {
+    """1163751742
+      |1381373672
+      |2136511328
+      |3694931569
+      |7463417111
+      |1319128137
+      |1359912421
+      |3125421639
+      |1293138521
+      |2311944581
+      |""".stripMargin.split("\n").toList
+  }
+}
