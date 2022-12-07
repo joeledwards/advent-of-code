1a1f5e1472f73a55cb4cc3f2b006e4e36c5d621a
Joel Edwards <joel.edwards@lark.com>
2022-12-07 01:09:33 -0700

    feat: ready for day 8

---
+++ 2022/src/main/scala/com/buzuli/advent/days/day8.scala
@@ -0,0 +1,21 @@
+package com.buzuli.advent.days
+
+import com.buzuli.advent.{AdventContext, AdventDay}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object day8 extends AdventDay(8) {
+  override def puzzles(implicit ec: ExecutionContext): List[AdventContext => Future[String]] = {
+    List(puzzle1, puzzle2)
+  }
+  def puzzle1(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future { p1.answer }
+  def puzzle2(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future { p2.answer }
+  
+  object p1 {
+    def answer: String = ""
+  }
+  
+  object p2 {
+    def answer: String = ""
+  }
+}
---
+++ 2022/src/test/scala/com/buzuli/advent/days/day8Spec.scala
@@ -0,0 +1,13 @@
+package com.buzuli.advent.days
+
+import com.buzuli.UnitSpec
+import com.buzuli.advent.days.day8._
+
+class day8Spec extends UnitSpec {
+  "day8._" when {
+    "" should {
+      "" in {
+      }
+    }
+  }
+}
