3ed8a6029c51a12adfcba90bea1ef9449f73711b
Joel Edwards <joel.edwards@lark.com>
2022-12-07 22:52:39 -0700

    feat: completed part 1 of Day 8

--- 2022/src/main/scala/com/buzuli/advent/days/day8.scala
+++ 2022/src/main/scala/com/buzuli/advent/days/day8.scala
@@ -11,8 +11,50 @@ object day8 extends AdventDay(8) {
   def puzzle1(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future { p1.answer }
   def puzzle2(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future { p2.answer }
   
+  def transposeGrid[T](grid: List[List[T]]): List[List[T]] = {
+    val width = grid(0).size
+    val height = grid.size
+    Range(0, width).toList map { j =>
+      Range(0, height).toList map { i =>
+        grid(i)(j)
+      }
+    }
+  }
+  
+  case class Tree(x: Int, y: Int, height: Int)
+  
+  type Grid = List[List[Tree]]
+  
+  def visibleTrees(grid: Grid): Set[Tree] = {
+    grid.foldLeft(Set.empty[Tree])({ (visible, row) =>
+      row.foldLeft((Set.empty[Tree], 0))({ (acc, tree) =>
+        val (visible, maxHeight) = acc
+        if (tree.height > maxHeight) {
+          (visible + tree, tree.height)
+        } else {
+          acc
+        }
+      })._1 | visible
+    })
+  }
+  
   object p1 {
-    def answer: String = ""
+    val grid: Grid = lines.zipWithIndex.map { case (r, x) =>
+      r.toCharArray.toList.zipWithIndex.map { case (h, y) =>
+        Tree(x, y, h.toInt)
+      }
+    }
+    
+    val top: Grid = transposeGrid(grid)
+    val left: Grid = grid
+    val right: Grid = left.map(_.reverse)
+    val bottom: Grid = top.map(_.reverse)
+    
+    val visible: Set[Tree] = {
+      visibleTrees(top) | visibleTrees(left) | visibleTrees(right) | visibleTrees(bottom)
+    }
+    
+    def answer: String = s"${visible.size}"
   }
   
   object p2 {
--- 2022/src/test/scala/com/buzuli/advent/days/day8Spec.scala
+++ 2022/src/test/scala/com/buzuli/advent/days/day8Spec.scala
@@ -4,9 +4,36 @@ import com.buzuli.UnitSpec
 import com.buzuli.advent.days.day8._
 
 class day8Spec extends UnitSpec {
-  "day8._" when {
-    "" should {
-      "" in {
+  "day8.transposeGrid" when {
+    "rotating nested lists" should {
+      "generate the expected rotation" in {
+        transposeGrid(List(
+          List(1, 2, 3),
+          List(4, 5, 6),
+          List(7, 8, 9),
+        )) shouldBe List(
+          List(1, 4, 7),
+          List(2, 5, 8),
+          List(3, 6, 9),
+        )
+        
+        transposeGrid(List(
+          List(1, 2),
+          List(3, 4),
+          List(5, 6),
+        )) shouldBe List(
+          List(1, 3, 5),
+          List(2, 4, 6),
+        )
+        
+        transposeGrid(List(
+          List(1, 2, 3),
+          List(4, 5, 6),
+        )) shouldBe List(
+          List(1, 4),
+          List(2, 5),
+          List(3, 6)
+        )
       }
     }
   }
