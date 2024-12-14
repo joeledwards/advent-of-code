c1928b38dfbedf4160aa9fb1d57702e54cc88582
Joel Edwards <joeledwards@gmail.com>
2024-12-13 18:18:22 -0700

    feat: finished day 4

--- 2024/src/main/scala/com/buzuli/advent/days/day4.scala
+++ 2024/src/main/scala/com/buzuli/advent/days/day4.scala
@@ -12,10 +12,108 @@ object day4 extends AdventDay(4) {
   }
 
   def puzzle1(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future {
-    s""
+    s"$xmasCount"
   }
   
   def puzzle2(context: AdventContext)(implicit ec: ExecutionContext): Future[String] = Future {
-    s""
+    s"$xMasCount"
+  }
+
+  case class Letter(x: Long, y: Long, value: String) {
+    def n: Letter  = copy(y=y+1)
+    def s: Letter  = copy(y=y-1)
+    def e: Letter  = copy(x=x+1)
+    def w: Letter  = copy(x=x-1)
+    def nw: Letter = copy(x=x-1, y=y+1)
+    def ne: Letter = copy(x=x+1, y=y+1)
+    def se: Letter = copy(x=x+1, y=y-1)
+    def sw: Letter = copy(x=x-1, y=y-1)
+
+    def masXCandidates(xMax: Long, yMax: Long): List[(Letter, Letter, Letter, Letter)] = {
+      if (value == "A") {
+        val candidates = List((nw, ne, se, sw))
+        candidates
+      } else {
+        Nil
+      }
+    }
+
+    def xmasVectorCandidates(xMax: Long, yMax: Long): List[(Letter, Letter, Letter)] = {
+      if (value == "X") {
+        val vector = {
+          (if (y+3 > yMax) Nil else List((n, n.n, n.n.n))) :::
+            (if (y-3 < 0)    Nil else List((s, s.s, s.s.s))) :::
+            (if (x+3 > xMax) Nil else List((e, e.e, e.e.e))) :::
+            (if (x-3 < 0)    Nil else List((w, w.w, w.w.w))) :::
+            (if (y+3 > yMax || x-3 < 0   ) Nil else List((nw, nw.nw, nw.nw.nw))) :::
+            (if (y+3 > yMax || x+3 > xMax) Nil else List((ne, ne.ne, ne.ne.ne))) :::
+            (if (y-3 < 0    || x-3 < 0   ) Nil else List((sw, sw.sw, sw.sw.sw))) :::
+            (if (y-3 < 0    || x+3 > xMax) Nil else List((se, se.se, se.se.se))) :::
+            Nil
+        }
+        vector
+      } else {
+        Nil
+      }
+    }
+
+    def key: (Long, Long) = x -> y
+  }
+
+  val letters = lines
+    .reverse
+    .zipWithIndex
+    .flatMap(yPair => {
+      val (line, y) = yPair
+      line
+        .split("")
+        .zipWithIndex
+        .map(xPair => {
+          val (letter, x) = xPair
+          Letter(x, y, letter)
+        })
+    })
+
+  val map = letters
+    .map(letter => letter.key -> letter)
+    .toMap
+
+  val xMax: Long = letters.map(_.x).max
+  val yMax: Long = letters.map(_.y).max
+
+  def xmasCount: Long = {
+    def checkVector(vector: (Letter, Letter, Letter)): Boolean = {
+      val (m, a, s) = vector
+      map.get(m.key).exists(_.value == "M") &&
+      map.get(a.key).exists(_.value == "A") &&
+      map.get(s.key).exists(_.value == "S")
+    }
+
+    letters
+      .flatMap(l => l.xmasVectorCandidates(xMax, yMax))
+      .count(checkVector)
+  }
+
+  def xMasCount: Long = {
+    def checkX(x: (Letter, Letter, Letter, Letter)): Boolean = {
+      val (a, b, c, d) = x
+      val letters = List(a, b, c, d).flatMap(l => map.get(l.key))
+      val ms = letters.filter(_.value == "M")
+      val ss = letters.filter(_.value == "S")
+
+      if (ms.size != 2)
+        false
+      else if (ss.size != 2)
+        false
+      else
+        ms match {
+          case List(a, b) if a.x == b.x || a.y == b.y => true
+          case _ => false
+        }
+    }
+
+    letters
+      .flatMap(l => l.masXCandidates(xMax, yMax))
+      .count(checkX)
   }
 }
